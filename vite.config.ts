88dc022a9a8cadd30c4ea381d43edd3fda6eae86
tomasi <tomasilv@home.local>
2026-01-21 23:28:38 +0800

    fix: update vite base url for new repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fear-greed-chart/',
+  base: '/fearandgreed_graphdata/',
   plugins: [react()],
   server: {
     proxy: {
